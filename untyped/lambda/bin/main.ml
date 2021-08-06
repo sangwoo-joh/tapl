f69e3de832d7a12c37802ab3999e7862334d6c1c
Sangwoo John <work.sangwoo.joh@gmail.com>
2021-08-06 13:07:05 +0900

    :construction: add main

--- untyped/lambda/bin/main.ml
+++ untyped/lambda/bin/main.ml
@@ -1,1 +1,33 @@
-let () = print_endline "Hello, World!"
+open Lambda
+
+let args =
+  [ ( "-I"
+    , Arg.String (fun file -> Driver.search_path := file :: !Driver.search_path)
+    , "Append a directory to the search path" ) ]
+
+
+let usage_msg = "[-I <dir>]* ... <file>"
+
+let parse_args () =
+  let file = ref (None : string option) in
+  Arg.parse args
+    (fun s ->
+      match !file with
+      | Some _ ->
+          raise (Invalid_argument "You must specify exactly one input file.")
+      | None ->
+          file := Some s)
+    usage_msg ;
+  match !file with
+  | None ->
+      raise (Invalid_argument "You must specify at least one input file.")
+  | Some s ->
+      s
+
+
+let main () =
+  let file = parse_args () in
+  ignore (Driver.process_file Lambda.Eval.empty_ctx file)
+
+
+let () = main ()
